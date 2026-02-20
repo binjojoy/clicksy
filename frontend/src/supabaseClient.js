ea96da568baf3e059678cc10997b575c66d1d7f5
TitanAxfr <binjojoy1@gmail.com>
2026-02-20 14:47:03 +0530

    removed unneccessary pages and styles to legacy directory

--- frontend/src/supabaseClient.js
+++
@@ -1,5 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-const SUPABASE_URL = "https://nsjovdsmratfqdmzetdg.supabase.co"; 
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5zam92ZHNtcmF0ZnFkbXpldGRnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk4NTc2MTUsImV4cCI6MjA3NTQzMzYxNX0.8hFneGiRFHspwDIurbeZzwbyCJSx8Nbu7mh7oDcPRJM";
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
